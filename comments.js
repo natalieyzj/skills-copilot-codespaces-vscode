34bf6245b46b19d5b818bf6ecdbed6f0f18af759
ZHAOJUN YE <838623641@qq.com>
2025-01-26 14:09:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,41 +1,39 @@
-// creat web serve
+// Create web serve
+// 1. import express
+// 2. create an express instance
+// 3. create the first route
+// 4. start the server
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// get comments
-app.get('/comments', (req, res) => {
-    res.send('GET comments!');
-});
-
-// post comments
-app.post('/comments', (req, res) => {
-    res.send('POST comments!');
-});
-
-// put comments
-app.put('/comments', (req, res) => {
-    res.send('PUT comments!');
-});
-
-// delete comments
-app.delete('/comments', (req, res) => {
-    res.send('DELETE comments!');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
-// run the server
-// node comments.js
-// Server is running on port 3000
+// 5. test the server
+// run the server by running node comments.js
+// go to a browser and type localhost:3000
+// you should see Hello World
 
-// GET comments!
-// POST comments!
-// PUT comments!
-// DELETE comments!
-// GET comments!
-// POST comments!
-// PUT comments!
-// DELETE comments!
+// 6. create a route that returns a list of comments
+app.get('/comments', (req, res) => {
+  res.json([
+    { id: 1, author: 'John Doe', body: 'This is a comment' },
+    { id: 2, author: 'Jane Doe', body: 'This is another comment' },
+  ]);
+});
+
+// 7. test the comments route
+// go to a browser and type localhost:3000/comments
+// you should see the list of comments
+
+// 8. create a route that returns a single comment
+app.get('/comments/:id', (req, res) => {
+  const comment = {
+    id: 1,
