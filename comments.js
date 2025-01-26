a1e34d2a51b947b58c2b4ea3d9a43cd4f3f8ae49
ZHAOJUN YE <838623641@qq.com>
2025-01-26 14:18:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,34 +1,13 @@
 // Create web serve
 
-// Import express
-const express = require('express');
-const router = express.Router();
-const { Comment } = require('../models/Comment');
+var express = require('express');
+var router = express.Router();
+var commentController = require('../controllers/commentController');
 
-//=================================
-//             Comments
-//=================================
-
-router.post('/saveComment', (req, res) => {
-    const comment = new Comment(req.body);
-    comment.save((err, comment) => {
-        if (err) return res.json({ success: false, err })
-        Comment.find({ '_id': comment._id })
-            .populate('writer')
-            .exec((err, result) => {
-                if (err) return res.json({ success: false, err })
-                return res.status(200).json({ success: true, result })
-            })
-    })
-});
-
-router.post('/getComments', (req, res) => {
-    Comment.find({ 'postId': req.body.videoId })
-        .populate('writer')
-        .exec((err, comments) => {
-            if (err) return res.json({ success: false, err })
-            return res.status(200).json({ success: true, comments })
-        })
-});
+/* GET users listing. */
+router.post('/create', commentController.createComment);
+router.get('/list', commentController.listComment);
+router.put('/update/:id', commentController.updateComment);
+router.delete('/delete/:id', commentController.deleteComment);
 
 module.exports = router;
